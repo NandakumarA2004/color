97e25ba028a573351eb021dc0f1245afcb647c1e
Nandakumar A <nandakumarpattoor2@gmail.com>
2025-09-18 19:55:52 +0530

    Update project files

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-    const changeclr=()=> {
-        let color = document.getElementById('colorSelect').value;
-        document.body.style.backgroundColor = color;
-        localStorage.setItem('selectedColor', color);
-    }
-    let savedColor = localStorage.getItem('selectedColor');
-    let select = document.getElementById('colorSelect');
-    if (savedColor && select) {
-        document.body.style.backgroundColor = savedColor;
-        select.value = savedColor;
-    }
-  
+const changeclr = () => {
+    let color = document.getElementById('colorSelect').value;
+    document.body.style.backgroundColor = color;
+    sessionStorage.setItem('selectedColor', color);
+}
+let savedColor = sessionStorage.getItem('selectedColor');
+let select = document.getElementById('colorSelect');
+if (savedColor && select) {
+    document.body.style.backgroundColor = savedColor;
+    select.value = savedColor;
+}
